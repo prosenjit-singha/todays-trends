0c6df5a15ad364c7f4ef740a7ee36561f90b1cc5
mahmudur-samy <mrahmansamy@gmail.com>
2022-02-19 13:52:19 +0600

    a slightly modified

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
 const alanKey =
   "9d1324a30a0a78d5a51fdfa0d05b9c372e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
       onCommand: ({ command, articles }) => {
         if (command === "newHeadlines") {
-          console.log(articles);
+          setNewsArticles(articles);
         }
-      },
-    });
+      }
+    })
   }, []);
-  return <div>Hi</div>;
+  return (
+    <div>
+      <h1>HELLO</h1>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Components/NewsCards/NewsCards.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NewsCards = ({articles}) => {
+    return (
+        <div>
+            {<h1>NewsCards</h1>}
+        </div>
+    )
+}
