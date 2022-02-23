7e4dc4c720dfa3c77065aecf400715e6fa665dd3
PJAheibam <prosenjitsingha28@gmail.com>
2022-02-23 12:11:24 +0600

    homepage

--- src/Components/NewsCards/styles.js
+++ src/Components/NewsCards/styles.js
@@ -1,10 +1,27 @@
 import { makeStyles } from "@material-ui/core/styles";
+import { withThemeCreator } from "@material-ui/styles";
 
 const styles = makeStyles({
   container: {
     padding: "0 5%",
     width: "100%",
     margin: "0",
+  },
+  card: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    height: "45 vh",
+    padding: "10",
+    borderRadius: 10,
+    color: "white",
+  },
+  infoCard: {
+    display: "flex",
+    flexDirection: "center",
+    textAlign: "center",
   },
 });
 
